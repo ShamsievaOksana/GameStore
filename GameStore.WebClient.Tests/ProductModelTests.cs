689ceda8918cea56287ec7487f9c9d2a75ec45f6
Oksana Abramchenko <shamsieva.oksana@gmail.com>
2021-10-15 00:02:53 +0300

    Added ProductModel unit tests

---
+++ GameStore.WebClient.Tests/ProductModelTests.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using FizzWare.NBuilder;
+using FluentAssertions;
+using GameStore.WebClient.Product;
+using NUnit.Framework;
+
+namespace GameStore.WebClient.Tests
+{
+    [TestFixture]
+    public class ProductModelTests
+    {
+        [Test]
+        public void Validate_PriceIsMissing_ShouldNotBeValid()
+        {
+            // Arrange
+            var productModel = Builder<ProductModel>
+                .CreateNew()
+                .With(x => x.Price, 0)
+                .Build();
+
+            // Act
+            var validationResult = productModel.Validate(new ValidationContext(productModel));
+            
+            // Assert
+            validationResult.Should().NotBeNullOrEmpty();
+            validationResult.FirstOrDefault(x => x.MemberNames.Contains("Price")).Should().NotBeNull();
+        }
+        
+        [Test]
+        public void Validate_PriceHasNegativeValue_ShouldNotBeValid()
+        {
+            // Arrange
+            var productModel = Builder<ProductModel>
+                .CreateNew()
+                .With(x => x.Price, -1)
+                .Build();
+
+            // Act
+            var validationResult = productModel.Validate(new ValidationContext(productModel));
+            
+            // Assert
+            validationResult.Should().NotBeNullOrEmpty();
+            validationResult.FirstOrDefault(x => x.MemberNames.Contains("Price")).Should().NotBeNull();
+        }
+    }
+}
--- GameStore.WebClient/Product/ProductModel.cs
+++ GameStore.WebClient/Product/ProductModel.cs
@@ -1,15 +1,29 @@
-﻿namespace GameStore.WebClient.Product
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace GameStore.WebClient.Product
 {
     public class ProductModel
+        : IValidatableObject
     {
         public int Id { get; set; }
 
+        [Required]
+        [MaxLength(100)]
         public string Name { get; set; }
-
+        
+        
         public decimal Price { get; set; }
 
+        [Required]
+        [MaxLength(1000)]
         public string Description { get; set; }
 
         public bool Published { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
