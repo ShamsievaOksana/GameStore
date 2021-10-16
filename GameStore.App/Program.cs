cfd291fc46421d98fa9414aa9c7f6b521ac097ce
Oksana Abramchenko <shamsieva.oksana@gmail.com>
2021-10-16 23:02:04 +0300

    Added loging to Console and ElasticSearch; Cleaned up Startup.

--- GameStore.App/Program.cs
+++ GameStore.App/Program.cs
@@ -1,23 +1,69 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Core;
 
 namespace GameStore.App
 {
     public class Program
     {
-        public static void Main(string[] args)
+        private static IConfiguration Configuration { get; } = BuildConfiguration();
+
+        public static int Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = CreateLogger();
+
+            try
+            {
+                Log.Information("Starting host...");
+                CreateHostBuilder(args).Build().Run();
+
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+        private static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((_, configBuilder) =>
+                {
+                    configBuilder.AddConfiguration(Configuration);
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                }).UseSerilog();
+        }
+        
+        private static IConfiguration BuildConfiguration()
+        {
+            var configurationBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true)
+                .AddEnvironmentVariables();
+
+            return configurationBuilder.Build();
+        }
+        
+        private static Logger CreateLogger()
+        {
+            var loggerConfiguration = new LoggerConfiguration()
+                .ReadFrom.Configuration(Configuration);
+
+            return loggerConfiguration.CreateLogger();
+        }
     }
 }
