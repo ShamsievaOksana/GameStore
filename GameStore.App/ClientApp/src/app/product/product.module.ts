ca1efa68d3e71123456ac85d23af411d7de8ec90
Oksana Abramchenko <shamsieva.oksana@gmail.com>
2021-10-17 12:55:16 +0300

    Styled products list and product details page with Angular Material.

--- GameStore.App/ClientApp/src/app/product/product.module.ts
+++ GameStore.App/ClientApp/src/app/product/product.module.ts
@@ -4,11 +4,19 @@ import { ProductComponent } from './product.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { RouterModule } from '@angular/router';
 import { ProductService } from './product-service';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatChipsModule } from '@angular/material/chips';
 
 @NgModule({
   declarations: [ProductComponent, ProductDetailsComponent],
   imports: [
     CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatBadgeModule,
+    MatChipsModule,
     RouterModule.forRoot([
       { path: 'products', component: ProductComponent },
       { path: 'products/:productId', component: ProductDetailsComponent }
